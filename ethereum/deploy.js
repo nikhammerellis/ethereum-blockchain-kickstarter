2b6f40fdf61999227e93e059bbc8340f8ca638d7
Nik <nikhammerellis@gmail.com>
2018-10-16 14:06:20 -0700

    automated campaign factory deployment, now builds a json file after deployment that factory.js uses to pull in the current address

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,5 +1,7 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
+const path = require("path");
+const fs = require("fs-extra");
 const compiledFactory = require("./build/CampaignFactory.json");
 const mnemonic = require("../secrets");
 
@@ -23,5 +25,19 @@ const deploy = async () => {
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("contract deployed to: ", result.options.address);
+  console.log("updating factory address...");
+  const addressPath = path.join(__dirname, "../factory-address.json");
+  //remove old address file
+  fs.removeSync(addressPath);
+  //create new address file
+  fs.ensureFileSync(addressPath);
+
+  let factoryAddress = {
+    address: result.options.address
+  };
+
+  let data = JSON.stringify(factoryAddress);
+  fs.writeFileSync(addressPath, data);
+  console.log("factory address updated");
 };
 deploy();
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,12 @@
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
+let addressData = require("../factory-address.json");
+
 //create an instance of our deployed factory contract
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x895151eafB2175D39376e90df6B30b4CCEBf65Af"
+  addressData.address
 );
 
 export default instance;
